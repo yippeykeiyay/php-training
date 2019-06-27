2c60507f51245f6695334ff33a9d2701c6e1f1b3
James Tyler <james.tyler@rapidspike.com>
2019-06-27 13:03:30 +0100

    fixed incorrect comment

--- Part1/create_car.php
+++ Part1/create_car.php
@@ -5,7 +5,7 @@
  */
 require_once __DIR__ . '/Car.php';
 
-// Create the engine object
+// Create the car object
 $objCar = new \Part1\Car('Mazda', '3', 2013, 5, 5, 1.6, 'petrol', 4);
 
 // Print the car object
