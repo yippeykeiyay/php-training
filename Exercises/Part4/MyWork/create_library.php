b7b7ce165c45cac064bbde8f1555c0710a587ce0
James Tyler <james.tyler@rapidspike.com>
2019-07-26 13:14:54 +0100

    added a bunch of functionality for handling adding/removing books to/from the library correctly

--- Exercises/Part4/MyWork/create_library.php
+++ Exercises/Part4/MyWork/create_library.php
@@ -8,16 +8,16 @@ $objLibrary = new Library();
 try {
     // Alien Book
     $objAlienBook = new Book(4345, 'Aliens & Their Friends', 'Martin Mathers', 901);
-
     $objLibrary->addBook($objAlienBook);
 
     // Sports Book
     $objSportsBook = new Book(554, 'Football Football Football, Hockey, Tennis...Golf', 'Unknown', 95);
-
     $objLibrary->addBook($objSportsBook);
 
-    print_r($objLibrary);
+    // Now remove the Sports Book
+    $objLibrary->removeBookById($objSportsBook->id);
 } catch (\Exception $e) {
     echo $e->getMessage(), PHP_EOL;
 }
 
+print_r($objLibrary);
--- Exercises/Part4/MyWork/src/Library.php
+++ Exercises/Part4/MyWork/src/Library.php
@@ -14,14 +14,71 @@ class Library
      */
     private $_arrBooks = array();
 
+    /**
+     * Add a book after checking it'll fit
+     *
+     * @param Book $objBook
+     *
+     * @return \Library
+     *
+     * @throws Exception
+     */
     public function addBook(Book $objBook): Library
     {
         if ((count($this->_arrBooks) + 1) > self::SIZE) {
             throw new Exception('Library is full :(');
         }
 
-        $this->_arrBooks[] = $objBook;
+        $this->_arrBooks[$objBook->id] = $objBook;
         return $this;
+    }
+
+    /**
+     * Get a Book object by its ID from the Library
+     *
+     * @param int $id
+     *
+     * @return \Book
+     */
+    public function getBookById(int $id): Book
+    {
+        if ($this->_hasBook($id)) {
+            return $this->_arrBooks[$id];
+        }
+    }
+
+    /**
+     * Remove a Book object from the Library by its ID
+     *
+     * @param int $id
+     *
+     * @return \Library
+     */
+    public function removeBookById(int $id): Library
+    {
+        if ($this->_hasBook($id)) {
+            unset($this->_arrBooks[$id]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Does the Library contain a Book?
+     *
+     * @param int $id
+     *
+     * @return bool
+     *
+     * @throws Exception
+     */
+    private function _hasBook(int $id): bool
+    {
+        if (!isset($this->_arrBooks[$id])) {
+            throw new Exception("Book #{$id} does not exist");
+        } else {
+            return true;
+        }
     }
 
 }
